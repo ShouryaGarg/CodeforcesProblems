60ce2d3e7a8306e4e99df74816309e8a7b352760
ShouryaGarg <77744661+ShouryaGarg@users.noreply.github.com>
2022-06-17 19:01:51 +0530

    Create Watermelon.cpp

---
+++ Watermelon.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define shourya cin
+#define garg cout
+#define musaib endl
+#define altaf int t; shourya>>t;
+ 
+int main(){
+    int w;
+    shourya>>w;
+    
+    if(w%2==0 && w>2){
+        garg<<"YES"<<musaib;
+    }
+    else{
+        garg<<"NO"<<musaib;
+    }
+    return 0;
+}
